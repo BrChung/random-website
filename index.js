ace6a8c4f5412f367229f56ecacf24963ce064a8
Brian Chung <35940686+BrChung@users.noreply.github.com>
2020-04-14 12:46:34 -0700

    Production

--- index.js
+++ index.js
@@ -5,8 +5,9 @@
  **/
 
 // URL to CloudFlare API to recieve URLs to variants
-const variantURLAPI =
-  "https://cfw-takehome.developers.workers.dev/api/variants";
+const VARIANT_API = "https://cfw-takehome.developers.workers.dev/api/variants";
+
+const COOKIE_NAME = "variant-id";
 
 // Upon 'fetch' from user, worker will respond with method provided by handleRequest
 addEventListener("fetch", event => {
@@ -18,38 +19,42 @@ addEventListener("fetch", event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  //Fetch CloudFlare api/variants and parse JSON
-  variantURLs = await (await fetch(variantURLAPI)).json();
+  variantURLs = await (await fetch(VARIANT_API)).json(); // Fetch CloudFlare api/variants JSON
+  const cookie = parseInt(getCookie(request, COOKIE_NAME)); //Get cookie (variant-id)
 
-  const cookie = getCookie(request, "variant-id");
-  if (cookie) {
-    variantNumber = cookie;
-    randChosenVariantURL = variantURLs.variants[variantNumber];
-    //Fetch variant HTML Response given random variant URL
-    response = await fetch(randChosenVariantURL);
-    //Return modified variant using HTMLRewriter
-    return variantRewriter.transform(response);
+  //* If cookie is valid (0, 1), rewrite variant *
+  if (cookie === 0 || cookie === 1) {
+    variantURL = variantURLs.variants[cookie]; // Get variant URL to be displayed given cookie
+    response = await fetch(variantURL); // Fetch variant HTML Response
+    return variantRewriter.transform(response); // Return modified variant using HTMLRewriter API
   }
-  //Since Math.random() generates a random float between 0 and 1, round to nearest int to get random index
-  variantNumber = Math.round(Math.random());
-  randChosenVariantURL = variantURLs.variants[variantNumber];
-  //Fetch variant HTML Response given random variant URL
-  response = await fetch(randChosenVariantURL);
+
+  //* Else cookie has not been previously set or is invalid *
+
+  // Math.random() generates a random float between 0 and 1, round to nearest int to get random index
+  variantID = Math.round(Math.random());
+
+  // Get Random Variant URL and fetch
+  randVariantURL = variantURLs.variants[variantID];
+  response = await fetch(randVariantURL);
 
   const transformedHTML = variantRewriter.transform(response);
 
+  // Create cookie <variant-id> that will expire in 24 hours
   const today = new Date();
-  const nextWeek = new Date(today.getTime() + 24 * 60 * 60 * 1000);
+  const tomorrow = new Date(today.getTime() + 24 * 60 * 60 * 1000);
+  const cookieHeader = COOKIE_NAME.concat(
+    "=",
+    variantID,
+    "; Expires=",
+    tomorrow.toUTCString(),
+    ";"
+  );
 
-  //Return modified variant using HTMLRewriter
+  // Return modified variant using HTMLRewriter and set new cookie
   return new Response(await transformedHTML.text(), {
     headers: {
-      "Set-Cookie": "variant-id=".concat(
-        variantNumber,
-        "; Expires=",
-        nextWeek.toUTCString(),
-        ";"
-      ),
+      "Set-Cookie": cookieHeader,
       "content-type": "text/html"
     }
   });
@@ -96,7 +101,7 @@ class ButtonHandler {
     const buttonText = text.text;
     const newButtonText = buttonText.replace(
       "Return to cloudflare.com",
-      "Go to brianchung.co"
+      "Continue to brianchung.co"
     );
     text.replace(newButtonText);
   }
@@ -122,6 +127,7 @@ const variantRewriter = new HTMLRewriter()
  * Grabs the cookie with name from the request headers
  * @param {Request} request incoming Request
  * @param {string} name of the cookie to grab
+ * Source: https://developers.cloudflare.com/workers/templates/pages/cookie_extract/
  */
 function getCookie(request, name) {
   let result = null;
